c77c8cd37235f475417e5f07d510dcf89976d252
Lars <lars@goodchee.com>
2025-02-11 12:57:35 -0500

    hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,7 +7,7 @@ const Hero: React.FC = () => {
         <img
           loading="lazy"
           src="https://cdn.builder.io/api/v1/image/assets/201c97aaaea7452ea91764ce57b99d71/a633ea89f6ae6450be83cb7924b9e3da219640cad313c5b0eddf8bb97100548e?apiKey=201c97aaaea7452ea91764ce57b99d71&"
-          className="object-contain w-full aspect-[3]"
+          className="object-cover w-full aspect-[3] h-[500px]"
           alt="Sentry Pest Solutions hero image showcasing pest control services"
         />
       </div>
